a2972dfe013c41fbac207c9a548067ffefbac88b
Suvra ghosh <103570957+suvraghosh@users.noreply.github.com>
2023-01-16 22:10:26 +0530

    Add files via upload

---
+++ Simple_GUI.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+imaginary_root=Tk()
+        #widthxheight
+imaginary_root.geometry("733x434")
+        #width,height
+imaginary_root.minsize(200,100)
+imaginary_root.maxsize(1000,400)
+s1=Label(text="PyCharm Community Edition")
+s1.pack()
+imaginary_root.mainloop()
