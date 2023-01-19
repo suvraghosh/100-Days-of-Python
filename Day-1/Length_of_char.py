dc7c912e80f7dc729c31a4ef4f2fa122c968d1a3
Suvra ghosh <103570957+suvraghosh@users.noreply.github.com>
2023-01-19 22:17:22 +0530

    Add files via upload

---
+++ Day-1/Length_of_char.py
@@ -0,0 +1,4 @@
+# print the length of a character given by the user
+
+name=input("What is your name? ")
+print(len(name))
---
+++ Day-1/Switching_input.py
@@ -0,0 +1,6 @@
+# A program that switches the values stored in the variables a and b.
+
+a = input("a: ")
+b = input("b: ")
+print("a:",b)
+print("b:",a)
