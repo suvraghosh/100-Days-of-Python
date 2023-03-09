5e6ec1b13f3f899c01ef99d66d7d7019c9c65ab5
Suvra ghosh <103570957+suvraghosh@users.noreply.github.com>
2023-03-09 22:44:06 +0530

    Web Scraping with Beautiful Soup

---
+++ Day-45/Top100_movies_scrap/scrap_movie.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+all_movies = soup.find_all(name="h3", class_="title")
+movie_titles = [movie.getText() for movie in all_movies]
+
+# printing the list of movies in reversed order
+
+"""
+for n in range(len(movie_titles)-1, -1, -1):
+    with open("movies.txt", mode="w") as file:
+        file.write(movie_titles[n])
+"""
+
+
+# Another way using slicing
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
